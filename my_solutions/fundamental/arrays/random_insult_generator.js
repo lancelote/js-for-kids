fe054830099a57e05152cf4b4cb6d5ef78cb14d1
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-07-06 21:35:40 +0300

    Do not hardcode length

--- my_solutions/fundamental/arrays/random_insult_generator.js
+++ my_solutions/fundamental/arrays/random_insult_generator.js
@@ -2,9 +2,9 @@ const randomBodyParts = ["Face", "Nose", "Hair"];
 const randomAdjectives = ["Smelly", "Boring", "Stupid"];
 const randomWords = ["Fly", "Marmot", "Stick", "Monkey", "Rat"];
 
-const randomBodyPart = randomBodyParts[Math.floor(Math.random() * 3)];
-const randomAdjective = randomAdjectives[Math.floor(Math.random() * 3)];
-const randomWord = randomWords[Math.floor(Math.random() * 5)];
+const randomBodyPart = randomBodyParts[Math.floor(Math.random() * randomBodyParts.length)];
+const randomAdjective = randomAdjectives[Math.floor(Math.random() * randomAdjectives.length)];
+const randomWord = randomWords[Math.floor(Math.random() * randomWords.length)];
 
 const randomInsult = `Your ${randomBodyPart} is like a ${randomAdjective} ${randomWord}!`;
 console.log(randomInsult);
